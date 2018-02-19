cc4a7e0edcee9467ca3bb0fe4a3f0a7be42179c4
JefersonPSilva <jeferson_p.silva@outlook.com>
2018-02-18 21:14:27 -0300

    Indexado novas classes no git

--- src/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ src/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -2,12 +2,16 @@ package br.com.alura.gerenciador.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.Empresa;
+import br.com.alura.gerenciador.dao.EmpresaDAO;
 
 @WebServlet(urlPatterns="/busca")
 public class BuscaEmpresa extends HttpServlet{
@@ -17,7 +21,16 @@ public class BuscaEmpresa extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter escrever = resp.getWriter();
 		escrever.println("<html><body>");
-		escrever.println("Resultado da busca:<br/>");
+		//escrever.println("Resultado da busca:<br/>");
+		
+		//Filtro = resultado -> Supermecado
+		String filter = "";
+		Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade(filter);
+		escrever.println("<ul>");
+		for(Empresa empresa : empresas) { 
+			escrever.println("<li>"+empresa.getId()+": "+ empresa.getNome()+"</li>");
+		}
+		escrever.println("</ul>");
 		
 		escrever.println("</body></html>");
 	}
--- src/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ src/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -1,5 +1,38 @@
 package br.com.alura.gerenciador.web;
 
-public class FiltroDeAuditoria {
+import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter(urlPatterns = "/*")
+public class FiltroDeAuditoria implements Filter{
+
+	@Override
+	public void destroy() {	
+		//Executado quando o filtro e destruido
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		//Cast de ServletRequest para HttpServletRequest 
+		HttpServletRequest req = (HttpServletRequest) request;
+		String uri = req.getRequestURI();
+		System.out.println("Usuario acesso a URI "+uri);
+		
+		//Continuar com a request e response
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {	
+		//Executado quando o filtro e inicializado
+	}
+	 
 }
