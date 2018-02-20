439785eb41d5b55c4684954a0272f90946217397
JefersonPSilva <jeferson_p.silva@outlook.com>
2018-02-20 20:09:31 -0300

    Usado session para corrigir falha de segurança nos cookie no logout

--- src/br/com/alura/gerenciador/web/Logout.java
+++ src/br/com/alura/gerenciador/web/Logout.java
@@ -13,17 +13,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns="/logout")
 public class Logout extends HttpServlet{
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Cookie cookie = new Cookies(req.getCookies()).buscoUsuarioLogado();
-		PrintWriter escrever = resp.getWriter();
-		if(cookie == null) { 
-			escrever.println("<html><body>Usuario não estava logado!</body></html>");
-			return;
-		}
-		//Encerra tempo usuário e desloga
-		cookie.setMaxAge(0);
-		//Envia como response o tempo do cookie zerado
-		resp.addCookie(cookie);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//métodos para finalizar uma sessão
+		request.getSession().removeAttribute("usuario.logado");
+		//request.getSession().invalidate();
+		
+		PrintWriter escrever = response.getWriter();
 		escrever.println("<html><body>Deslogado com sucesso!</body></html>");
 	}
 }
