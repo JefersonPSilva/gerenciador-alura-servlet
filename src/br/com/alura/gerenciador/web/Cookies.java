2abc997b1cdda97607bc997193c64370044aea9b
JefersonPSilva <jeferson_p.silva@outlook.com>
2018-02-20 17:00:22 -0300

    Implementado metodos para controle dos cookies

--- src/br/com/alura/gerenciador/web/Cookies.java
+++ src/br/com/alura/gerenciador/web/Cookies.java
@@ -1,12 +1,5 @@
 package br.com.alura.gerenciador.web;
 
-import javax.servlet.http.Cookie;
-
 public class Cookies {
-	private final Cookie[] cookies;
-	
-	//Construtor
-	public Cookies(Cookie[] cookies) { 
-		this.cookies = cookies;
-	}
+
 }
