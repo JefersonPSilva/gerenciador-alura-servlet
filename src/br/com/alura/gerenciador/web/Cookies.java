ed5cef54d9044edd792d86bc2c71fe93772af0e9
JefersonPSilva <jeferson_p.silva@outlook.com>
2018-02-20 16:59:22 -0300

    Criado class e metodo para controle dos cookies.

---
+++ src/br/com/alura/gerenciador/web/Cookies.java
@@ -0,0 +1,12 @@
+package br.com.alura.gerenciador.web;
+
+import javax.servlet.http.Cookie;
+
+public class Cookies {
+	private final Cookie[] cookies;
+	
+	//Construtor
+	public Cookies(Cookie[] cookies) { 
+		this.cookies = cookies;
+	}
+}
