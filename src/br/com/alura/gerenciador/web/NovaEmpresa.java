1f69012c3917bcdbe07b04f7ebc20d2892cc11d8
JefersonPSilva <jeferson_p.silva@outlook.com>
2018-02-25 16:02:42 -0300

    Implementado interface tarefa e metodo executa para novaEmpresa

--- src/br/com/alura/gerenciador/web/NovaEmpresa.java
+++ src/br/com/alura/gerenciador/web/NovaEmpresa.java
@@ -1,31 +1,21 @@
 package br.com.alura.gerenciador.web;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.alura.gerenciador.Empresa;
 import br.com.alura.gerenciador.dao.EmpresaDAO;
 
-@WebServlet(urlPatterns = "/novaEmpresa")
-public class NovaEmpresa extends HttpServlet {
+public class NovaEmpresa implements Tarefa {
 	
-	private static final long serialVersionUID = 1L;
-
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public String executa(HttpServletRequest request, HttpServletResponse response) {
 		String nome = request.getParameter("nome");
 		Empresa empresa = new Empresa(nome);
 		new EmpresaDAO().adiciona(empresa);
 		
 		request.setAttribute("empresa", empresa);
 		
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/paginas/novaEmpresa.jsp");
-		dispatcher.forward(request, response);
+		return "/WEB-INF/paginas/novaEmpresa.jsp";
 	}
 }
