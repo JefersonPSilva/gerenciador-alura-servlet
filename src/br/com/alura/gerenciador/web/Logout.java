791b7d3bd4b6ed201788944a2a8ccfbb92697c78
JefersonPSilva <jeferson_p.silva@outlook.com>
2018-02-25 15:42:07 -0300

    Implementado interface Tarefa paga logout e metodo executa

--- src/br/com/alura/gerenciador/web/Logout.java
+++ src/br/com/alura/gerenciador/web/Logout.java
@@ -1,26 +1,16 @@
 package br.com.alura.gerenciador.web;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns="/logout")
-public class Logout extends HttpServlet{
-
-	private static final long serialVersionUID = 697426254938121928L;
+public class Logout implements Tarefa{
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public String executa(HttpServletRequest request, HttpServletResponse response) {
 		//métodos para finalizar uma sessão
 		request.getSession().removeAttribute("usuarioLogado");
 		//request.getSession().invalidate();
 		//Redireciona o cliente para a página de logout no lado do servidor
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/paginas/logout.html");
-		dispatcher.forward(request, response);
+		return "/WEB-INF/paginas/logout.html";
 	}
 }
