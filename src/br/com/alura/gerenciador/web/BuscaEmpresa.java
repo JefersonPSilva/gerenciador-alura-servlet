f914ca0416fae1bcde0dc18893a7b72c275494c5
JefersonPSilva <jeferson_p.silva@outlook.com>
2018-02-25 15:37:25 -0300

    Implementado interface Tarefa e metodo executa

--- src/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ src/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -1,32 +1,20 @@
 package br.com.alura.gerenciador.web;
 
-import java.io.IOException;
 import java.util.Collection;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.alura.gerenciador.Empresa;
 import br.com.alura.gerenciador.dao.EmpresaDAO;
 
-@WebServlet(urlPatterns="/busca")
-public class BuscaEmpresa extends HttpServlet{
-	private static final long serialVersionUID = 1L;
-
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class BuscaEmpresa implements Tarefa{
+	public String executa(HttpServletRequest request, HttpServletResponse response) {
 		String filtro = request.getParameter("filtro");
 		
 		Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade(filtro);
 		
 		request.setAttribute("empresas", empresas);
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/paginas/buscaEmpresa.jsp");
-		
-		dispatcher.forward(request, response);
-		
+		return "/WEB-INF/paginas/buscaEmpresa.jsp";
 	}
 }
