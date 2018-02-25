b4988c46d02440803347850568644005c3cce888
JefersonPSilva <jeferson_p.silva@outlook.com>
2018-02-25 15:31:28 -0300

    Implemtando logica em service para trabalhar com request e response

--- src/br/com/alura/gerenciador/web/FazTudo.java
+++ src/br/com/alura/gerenciador/web/FazTudo.java
@@ -2,6 +2,7 @@ package br.com.alura.gerenciador.web;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,13 +11,29 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns="/fazTudo")
 public class FazTudo extends HttpServlet{
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
-	
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private static final long serialVersionUID = 6626322586019870362L;
 
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String tarefa = request.getParameter("tarefa");
+		
+		if(tarefa == null) { 
+			throw new IllegalArgumentException("Você esqueceu de passar a tarefa");
+		}
+		//Pega a string tarefa
+		tarefa = "br.com.alura.gerenciador.web." + tarefa;
+		try {
+			//Pega a class da string tarefa
+			Class<?> tipo = Class.forName(tarefa);
+			//Interace que representa a instancia da class tarefa
+			Tarefa instancia = (Tarefa) tipo.newInstance();
+			//Chama o metodo executa
+			String pagina = instancia.executa(request, response);
+			//Redireciona para a view
+			RequestDispatcher dispatcher = request.getRequestDispatcher(pagina);
+			dispatcher.forward(request, response);
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+			throw new ServletException(e);
+		}
 	}
 }
