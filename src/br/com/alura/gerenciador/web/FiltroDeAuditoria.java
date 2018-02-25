7a4aab1aa7ef98a14a1f414f276a61aa6566e598
JefersonPSilva <jeferson_p.silva@outlook.com>
2018-02-25 14:58:21 -0300

    Modificado referência do atributo para sessão de login do usuário

--- src/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ src/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -9,7 +9,6 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import br.com.alura.gerenciador.Usuario;
@@ -43,7 +42,7 @@ public class FiltroDeAuditoria implements Filter{
 	
 	//pega o usuário na sessão iniciada
 	private String getUsuario(HttpServletRequest req) {
-		Usuario usuario = (Usuario) req.getSession().getAttribute("usuario.logado");
+		Usuario usuario = (Usuario) req.getSession().getAttribute("usuarioLogado");
 		if(usuario == null) return "<deslogado>";
 		return usuario.getEmail();
 	}
